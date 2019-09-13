6336e684d4953b886dfc09f303e9fd016df15941
DELL <DELL@DESKTOP-8DUC6NQ>
2019-09-12 20:15:14 -0600

    Proyecto Odontologia Avance 3

---
+++ SistemaOdontologico/Menu.cs
@@ -0,0 +1,79 @@
+﻿using SistemaOdontologico.BL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaOdontologico
+{
+    public partial class Menu : Form
+
+    {
+        ClienteBL _clientesBL;
+        ServiciosBL _serviciosBL;
+        TratamientoBL _tratamientosBL;
+        FacturaBL _facturasBL;
+        UsuariosBL _usuariosBL;
+
+        public Menu()
+        {
+            InitializeComponent();
+            _clientesBL = new ClienteBL();
+            _serviciosBL = new ServiciosBL();
+            _tratamientosBL = new TratamientoBL();
+            _facturasBL = new FacturaBL();
+            _usuariosBL = new BL.UsuariosBL();
+
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formclientes = new Clientes();
+            formclientes.MdiParent = this;
+            formclientes.CargarDatos(_clientesBL, _serviciosBL);
+            formclientes.Show();
+        }
+
+        private void serviciosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formtratamiento = new Tratamientos ();
+            formtratamiento.MdiParent = this;
+            formtratamiento.CargarDatos(_tratamientosBL);
+            formtratamiento.Show();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+                Application.Exit();
+           
+        }
+
+        private void facturasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formFactura = new Form1();
+            formFactura.MdiParent = this;
+
+            formFactura.CargarDatos(_facturasBL, _clientesBL, _tratamientosBL);
+
+            formFactura.Show();
+
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            var login = new login();
+            login.CargarDatos(_usuariosBL);
+            login.ShowDialog();
+
+            facturasToolStripMenuItem.Visible = login.UsuarioAutenticado.PuedeVerFacturas;
+            serviciosToolStripMenuItem.Visible = login.UsuarioAutenticado.PuedeVerServicio;
+            clientesToolStripMenuItem.Visible = login.UsuarioAutenticado.PuedeVerCliente;
+        }
+    }
+}
